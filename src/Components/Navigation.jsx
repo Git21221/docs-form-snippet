fe0bb5cc76048174d8a29213d3feabf05a307cd1
Sunny Mishra <sunnymishra9007689708@gmail.com>
2024-08-30 23:25:31 +0530

    added github link

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaGithub, FaMagnifyingGlass } from "react-icons/fa6";
 import { IoMoonOutline, IoSunnyOutline } from "react-icons/io5";
 
@@ -15,7 +15,9 @@ const Navigation = () => {
             <div id="searchbar" className="w-64 h-11 bg-slate-200 rounded-full flex items-center justify-between p-2 cursor-pointer select-none"><FaMagnifyingGlass className="text-slate-600"/><span className="text-slate-600 font-medium">Search</span><span className="font-bold bg-white p-1 text-[12px] rounded-full">Ctrl + Q</span></div>
             <div id="others">
                 <div className="text-black text-2xl font-bold flex gap-5">
+                    <Link to='https://github.com/Git21221/form-snippet' target="_blank">
                     <FaGithub className="cursor-pointer" />
+                    </Link>
                     {
                         theme ?
                             <IoMoonOutline className="cursor-pointer" onClick={toggleTheme} />
